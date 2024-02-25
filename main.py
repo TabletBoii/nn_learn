de8f5d6dcf3534539da9c4491c22c502fe0f013c
TabletBoii <grand1354ru@gmail.com>
2024-02-25 07:18:34 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import nnfs
+import numpy as np
+from nnfs.datasets import spiral_data
+nnfs.init()
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = np.maximum(0, inputs)
+print(output)
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+
+activation1 = Activation_ReLU()
+
+dense1.forward(X)
+
+activation1.forward(dense1.output)
+
+print(activation1.output[:5])
